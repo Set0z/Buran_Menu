20519669ccf291fea89871de1f8413613db5948e
Setoz <markiss226@gmail.com>
2025-03-20 12:03:14 +0300

    Update Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -1,3 +1,2 @@
 @echo off
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v "ExecutionPolicy" /t REG_SZ /d "Bypass" /f
-powershell -Command "Start-Process powershell -ArgumentList '-File \"%~dp0\modules\script.ps1\"' -Verb runAs"
+powershell -Command "Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File \"%~dp0\modules\script.ps1\"' -Verb runAs"
